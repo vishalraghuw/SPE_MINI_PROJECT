aca34e681664cad3a452c54c659aa95ce228c23d
vishalraghuw <91356875+vishalraghuw@users.noreply.github.com>
2024-02-23 23:16:24 +0530

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 
-public class Main {
+public class main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
