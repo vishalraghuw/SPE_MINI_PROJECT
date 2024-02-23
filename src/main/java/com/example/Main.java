68d6c5780486c50d0c8eb10503c8da4361f85809
vishalraghuw <91356875+vishalraghuw@users.noreply.github.com>
2024-02-23 23:20:31 +0530

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,9 +2,9 @@ package org.example;
 
 import java.util.Scanner;
 
-public class main {
+public class Main {
 
-    public static void main(String[] args) {
+    public static void Main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to Basic Calculator!");
