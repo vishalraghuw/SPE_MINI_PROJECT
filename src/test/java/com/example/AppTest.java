9ff3b8e1b7d865c3285b0bb3fad87602e3bfd16c
vishalraghuw <91356875+vishalraghuw@users.noreply.github.com>
2024-02-24 15:29:17 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.calculator;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
@@ -6,57 +6,57 @@ import static org.junit.Assert.assertNotEquals;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for CalculatorApp.
  */
-public class AppTest {
+public class CalculatorAppTest {
     private static final double DELTA = 1e-15;
-    App calculator = new App();
+    CalculatorApp calculator = new CalculatorApp();
 
     @Test
     public void testFactorialWithPositiveNumbers() {
-        assertEquals("Factorial of 6", 720, calculator.fact(6), DELTA);
-        assertEquals("Factorial of 0", 1, calculator.fact(0), DELTA);
+        assertEquals("Factorial of 6", 720, calculator.factorial(6), DELTA);
+        assertEquals("Factorial of 0", 1, calculator.factorial(0), DELTA);
     }
 
     @Test
     public void testFactorialWithNegativeNumbers() {
-        assertNotEquals("Factorial of 5", 113, calculator.fact(5), DELTA);
-        assertNotEquals("Factorial of 6", 10, calculator.fact(6), DELTA);
+        assertNotEquals("Factorial of 5", 113, calculator.factorial(5), DELTA);
+        assertNotEquals("Factorial of 6", 10, calculator.factorial(6), DELTA);
     }
 
     @Test
     public void testPowerWithPositiveNumbers() {
-        assertEquals("Power of 3^4", 81, calculator.power(3, 4), DELTA);
-        assertEquals("Power of 5^2", 25, calculator.power(5, 2), DELTA);
+        assertEquals("Power of 3^4", 81, calculator.calculatePower(3, 4), DELTA);
+        assertEquals("Power of 5^2", 25, calculator.calculatePower(5, 2), DELTA);
     }
 
     @Test
     public void testPowerWithNegativeNumbers() {
-        assertNotEquals("Power of 2^3", 6, calculator.power(2, 3), DELTA);
-        assertNotEquals("Power of 3^3", -7, calculator.power(3, 3), DELTA);
+        assertNotEquals("Power of 2^3", 6, calculator.calculatePower(2, 3), DELTA);
+        assertNotEquals("Power of 3^3", -7, calculator.calculatePower(3, 3), DELTA);
     }
 
     @Test
     public void testNaturalLogWithPositiveNumbers() {
-        assertEquals("Natural log of 1", 0, calculator.naturalLog(1), DELTA);
-        assertEquals("Natural log of e", 1, calculator.naturalLog(2.718281828459045), DELTA);
+        assertEquals("Natural log of 1", 0, calculator.calculateNaturalLog(1), DELTA);
+        assertEquals("Natural log of e", 1, calculator.calculateNaturalLog(2.718281828459045), DELTA);
     }
 
     @Test
     public void testNaturalLogWithNegativeNumbers() {
-        assertNotEquals("Natural log of 2.4", 60, calculator.naturalLog(2.4), DELTA);
-        assertNotEquals("Natural log of 2.1", 4.7, calculator.naturalLog(2.1), DELTA);
+        assertNotEquals("Natural log of 2.4", 60, calculator.calculateNaturalLog(2.4), DELTA);
+        assertNotEquals("Natural log of 2.1", 4.7, calculator.calculateNaturalLog(2.1), DELTA);
     }
 
     @Test
     public void testSquareRootWithPositiveNumbers() {
-        assertEquals("Square root of 16", 4, calculator.squareRoot(16), DELTA);
-        assertEquals("Square root of 36", 6, calculator.squareRoot(36), DELTA);
+        assertEquals("Square root of 16", 4, calculator.calculateSquareRoot(16), DELTA);
+        assertEquals("Square root of 36", 6, calculator.calculateSquareRoot(36), DELTA);
     }
 
     @Test
     public void testSquareRootWithNegativeNumbers() {
-        assertNotEquals("Square root of 3", 1, calculator.squareRoot(3), DELTA);
-        assertNotEquals("Square root of 81", 4, calculator.squareRoot(81), DELTA);
+        assertNotEquals("Square root of 3", 1, calculator.calculateSquareRoot(3), DELTA);
+        assertNotEquals("Square root of 81", 4, calculator.calculateSquareRoot(81), DELTA);
     }
 }
